1a077473679ac84054c7ef717ddba68ecb1cfbbc
LuisSubiabre <luis.subiabre@gmail.com>
2024-11-20 12:30:12 -0300

    put and delete method

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const fs = require('fs');
+const cors = require('cors');
 
 const app = express();
 const fileName = 'repertorio.json';
 
 app.use(express.json()); //Middleware para transformar el body en json
-
+app.use(cors()); //Middleware para habilitar CORS
 
 /* comprueba archivo */
 const checkFile = () => {
@@ -15,25 +16,61 @@ const checkFile = () => {
 }
 
 /* escribe en archivo */
-const writeFile = (data) => {
+const writeFile = (method, data) => {
+    checkFile();
+    console.log(method, data);
     const datafile = JSON.parse(fs.readFileSync(fileName, 'utf8'));
-    datafile.push(data);
+    if (method === 'post') {
+        datafile.push(data);
+        fs.writeFileSync(fileName, JSON.stringify(datafile));
+    }
+    if (method === 'delete') {
 
-    fs.writeFileSync(fileName, JSON.stringify(datafile));
+        fs.writeFileSync(fileName, JSON.stringify(data));
+    }
+    if (method === 'put') {
+        fs.writeFileSync(fileName, JSON.stringify(data));
+    }
+
 };
+
+/* lee archivo */
+const readFile = () => {
+    checkFile();
+    return JSON.parse(fs.readFileSync(fileName, 'utf8'));
+}
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
 
+
+/* Logica y rutas */
 app.get('/canciones', (req, res) => {
-    res.send('hello word');
-    checkFile();
+    const datafile = readFile();
+    res.json(datafile); //Envia la respuesta en formato json del listado de canciones
 });
 
 app.post('/canciones', (req, res) => {
-    checkFile();
-    //res.json(req.body);
-    writeFile(req.body);
+    writeFile('post', req.body);
+});
+
+app.delete('/canciones/:id', (req, res) => {
+    const { id } = req.params; //destructuring id
+    const datafile = readFile(); //lee archivo
+    const index = datafile.filter((d) => d.id !== Number(id)); //filtra por id
+    writeFile('delete', index);
+    res.send('DELETE request to the homepage');
+
+});
+
+app.put('/canciones/:id', (req, res) => {
+    const { id } = req.params;
+    const cancion = req.body;
+    const datafile = readFile(); //lee archivo
+    const index = datafile.findIndex(p => p.id == id);
+    datafile[index] = cancion;
+    writeFile('put', datafile);
+    res.send('UPDATE request to the homepage');
 
 });
